b153e7006987d5bfcb1b812954965bdb91ddb20e
ylyf1994 <billzhang001@yeah.net>
2015-01-21 11:54:02 +0800

    to create the project

---
+++ ylyf-shudu/app/build/generated/source/buildConfig/release/com/dzy/ylyf/ylyf_shudu/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.dzy.ylyf.ylyf_shudu;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.dzy.ylyf.ylyf_shudu";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
